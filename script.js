9bfbe1d16db8d0926cf90d290e04ca786052e381
Glitch (glitch-hello-website) <none>
2023-08-09 21:56:54 +0000

    🕴⏱ Checkpoint ./index.html:59391617/4245 ./script.js:59391617/202 ./style.css:59391617/6782 ./.glitch-assets:59391617/405

--- script.js
+++
@@ -1,6 +0,0 @@
-<script>
-  var image = document.getElementById("myImage");
-  image.addEventListener("click", function() {
-    window.location.href = "https://www.instagram.com/your_instagram_username";
-  });
-</script>
