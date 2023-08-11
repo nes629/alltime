aee287da806d64ddde11329e11d391fe3a6520c6
Glitch (glitch-hello-website) <none>
2023-08-11 21:48:44 +0000

    🏛🐂 Checkpoint ./index.html:59391617/536 ./style.css:59391617/1393 ./script.js:59391617/1376 ./.glitch-assets:59391617/389

---
+++ script.js
@@ -0,0 +1,18 @@
+const slidesContainer = document.querySelector('.slideshow');
+const slides = slidesContainer.querySelectorAll('img');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    const slideOffset = (i - index) * 100; // Assuming each slide is 100% width
+    slide.style.transform = `translateX(${slideOffset}%)`; // Slide the image
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
+showSlide(currentSlide);
